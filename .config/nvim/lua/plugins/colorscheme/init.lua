14a0af79655875f89582cab0cfe80d20114df8ad
angelinuxx <me@angelopuddu.com>
2023-11-13 00:09:24 +0100

    Remove gruvbox from nvim

--- .config/nvim/lua/plugins/colorscheme/init.lua
+++ .config/nvim/lua/plugins/colorscheme/init.lua
@@ -1,27 +1,8 @@
 return {
-  { 
-    'rose-pine/neovim', 
-    name = 'rose-pine', 
-    priority = 1000, 
-    lazy = false,
-  },
   {
-    "folke/styler.nvim",
-    event = "VeryLazy",
-    config = function()
-      require("styler").setup {
-        themes = {
-          markdown = { colorscheme = "gruvbox" },
-          help = { colorscheme = "gruvbox" },
-        },
-      }
-    end,
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
+    "rose-pine/neovim",
+    name = "rose-pine",
+    priority = 1000,
     lazy = false,
-    config = function()
-      require("gruvbox").setup()
-    end,
   },
 }
