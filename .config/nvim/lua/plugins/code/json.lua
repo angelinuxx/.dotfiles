39fd64395c6855c700691a49baf53dc1e54688b9
angelinuxx <me@angelopuddu.com>
2024-09-18 09:56:50 +0200

    feat(nvim): add keymaps for json sorting

--- .config/nvim/lua/plugins/code/json.lua
+++ .config/nvim/lua/plugins/code/json.lua
@@ -6,5 +6,11 @@ return {
     "SortJSONByKeyLength",
     "SortJSONByKeyLengthReverse",
   },
+  keys = {
+    { "<leader>js", "<cmd>SortJSONByAlphaNum<cr>", desc = "Sort JSON by alpha num" },
+    { "<leader>jS", "<cmd>SortJSONByAlphaNumReverse<cr>", desc = "Sort JSON by alpha num reverse" },
+    { "<leader>jk", "<cmd>SortJSONByKeyLength<cr>", desc = "Sort JSON by key length" },
+    { "<leader>jK", "<cmd>SortJSONByKeyLengthReverse<cr>", desc = "Sort JSON by key length reverse" },
+  },
   config = true,
 }
