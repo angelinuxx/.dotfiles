7493fb02316fa5f516d590ff86a3b2bf65764a60
angelinuxx <me@angelopuddu.com>
2024-10-03 16:31:03 +0200

    feat(nvim): change multicursors plugin

--- .config/nvim/lua/plugins/multicursor.lua
+++ .config/nvim/lua/plugins/multicursor.lua
@@ -1,16 +1,31 @@
 return {
-  "smoka7/multicursors.nvim",
-  dependencies = {
-    "smoka7/hydra.nvim",
-  },
-  opts = {},
-  cmd = { "MCstart", "MCvisual", "MCclear", "MCpattern", "MCvisualPattern", "MCunderCursor" },
-  keys = {
-    {
-      mode = { "v", "n" },
-      "<c-m>",
-      "<cmd>MCstart<cr>",
-      desc = "Create a selection for selected text or word under the cursor",
+  {
+    "brenton-leighton/multiple-cursors.nvim",
+    version = "*", -- Use the latest tagged version
+    opts = {
+      match_visible_only = false,
+      pre_hook = function()
+        require("nvim-autopairs").disable()
+        require("cmp").setup { enabled = false }
+      end,
+      post_hook = function()
+        require("nvim-autopairs").enable()
+        require("cmp").setup { enabled = true }
+      end,
+    }, -- This causes the plugin setup function to be called
+    keys = {
+      { "<A-j>", "<Cmd>MultipleCursorsAddDown<CR>", mode = { "n", "x" }, desc = "Add cursor and move down" },
+      { "<A-k>", "<Cmd>MultipleCursorsAddUp<CR>", mode = { "n", "x" }, desc = "Add cursor and move up" },
+
+      { "<A-LeftMouse>", "<Cmd>MultipleCursorsMouseAddDelete<CR>", mode = { "n", "i" }, desc = "Add or remove cursor" },
+
+      { "<Leader>ac", "<Cmd>MultipleCursorsAddMatches<CR>", mode = { "n", "x" }, desc = "Add cursors to cword" },
+      { "<Leader>Ac", "<Cmd>MultipleCursorsAddMatchesV<CR>", mode = { "n", "x" }, desc = "Add cursors to cword in previous area" },
+
+      { "<A-m>", "<Cmd>MultipleCursorsAddJumpNextMatch<CR>", mode = { "n", "x" }, desc = "Add cursor and jump to next cword" },
+      { "<A-s>", "<Cmd>MultipleCursorsJumpNextMatch<CR>", mode = { "n", "x" }, desc = "Jump to next cword" },
+
+      { "<Leader>l", "<Cmd>MultipleCursorsLock<CR>", mode = { "n", "x" }, desc = "Lock virtual cursors" },
     },
   },
 }
