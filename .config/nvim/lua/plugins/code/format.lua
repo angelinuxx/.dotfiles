4b80416801833ba8920f15c138981072a1196616
angelinuxx <me@angelopuddu.com>
2024-11-09 01:18:52 +0100

    refactor(nvim): disable format on save

--- .config/nvim/lua/plugins/code/format.lua
+++ .config/nvim/lua/plugins/code/format.lua
@@ -72,34 +72,37 @@ return {
       },
       format_on_save = function(bufnr)
         -- Disable with a global or buffer-local variable
-        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-          return
+        if vim.g.enable_autoformat or vim.b[bufnr].enable_autoformat then
+          return {
+            lsp_fallback = true,
+            async = false,
+            timeout_ms = 5000,
+          }
         end
-        return {
-          lsp_fallback = true,
-          async = false,
-          timeout_ms = 5000,
-        }
       end,
     }
 
     -- User command to toggle autoformatting on save
+    vim.api.nvim_create_user_command("FormatEnable", function(args)
+      if args.bang then
+        -- FormatEnable! will enable formatting just for this buffer
+        vim.b.enable_autoformat = true
+      else
+        vim.g.enable_autoformat = true
+      end
+    end, {
+      desc = "Enable autoformat-on-save",
+      bang = true,
+    })
     vim.api.nvim_create_user_command("FormatDisable", function(args)
       if args.bang then
         -- FormatDisable! will disable formatting just for this buffer
-        vim.b.disable_autoformat = true
+        vim.b.enable_autoformat = false
       else
-        vim.g.disable_autoformat = true
+        vim.g.enable_autoformat = false
       end
     end, {
       desc = "Disable autoformat-on-save",
-      bang = true,
-    })
-    vim.api.nvim_create_user_command("FormatEnable", function()
-      vim.b.disable_autoformat = false
-      vim.g.disable_autoformat = false
-    end, {
-      desc = "Re-enable autoformat-on-save",
     })
 
     vim.keymap.set({ "n", "v" }, "<leader>cf", function()
